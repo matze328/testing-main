8928e6eb3f370ea88280d1dee0547e9fc9847728
matze328 <152269645+matze328@users.noreply.github.com>
2024-03-04 15:01:52 +0100

    3 tests max function Aufgabe2.2

--- unit-testing/minMax.test.js
+++ unit-testing/minMax.test.js
@@ -13,3 +13,15 @@ test("Kleinerer Wert zwischen einer negativen und einer positiven Zahl", () => {
     const result = minMax.min(4, 4);
     expect(result).toBe(4);
 });
+test("Größerer Wert zwischen zwei negativen Zahlen", () => {
+    const result = minMax.max(-3, -6);
+    expect(result).toBe(-3);
+  })
+test("Größerer Wert zwischen einer positiven und einer negativen Zahl", () => {
+    const result = minMax.max(4, -2);
+    expect(result).toBe(4);
+  });
+test("Größerer Wert zwischen zwei positiven Zahlen", () => {
+    const result = minMax.max(8, 12);
+    expect(result).toBe(12);
+});  
