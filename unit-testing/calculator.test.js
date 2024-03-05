5458c4c9b5adb6ed32230c648740fe87a0a0a451
matze328 <152269645+matze328@users.noreply.github.com>
2024-03-05 14:10:04 +0100

    Aufgabe 1 alle Test in Arrange, Act, Assert aufgeteilt

--- unit-testing/calculator.test.js
+++ unit-testing/calculator.test.js
@@ -1,66 +1,108 @@
 const calculator = require("./calculator");
 
 test("Die Summe von 2 + 5 ist 7", () => {
-  const result = calculator.sum(2, 5);
-
+  //Arrage
+  const [a, b] = [2, 5]
+  //Act
+  const result = calculator.sum(a, b);
+   // Assert (bzw. Expect)
   expect(result).toEqual(7);
 });
 
 test('Die Summe von "Apfel" + 5 ist "Apfel5"', () => {
-  const result = calculator.sum("Apfel", 5);
-
+    //Arrage
+  const [a, b] = ["Apfel", 5]
+    //Act
+  const result = calculator.sum(a, b);
+    // Assert (bzw. Expect)
   expect(result).toEqual("Apfel5");
 });
 
 test("Der Quotient aus 10 durch 2 ist 5", () => {
-  const result = calculator.divide(10, 2);
-
+      //Arrage
+  const [a, b] = [10, 2]
+    //Act
+  const result = calculator.divide(a, b);
+    // Assert (bzw. Expect)
   expect(result).toEqual(5);
 });
 
 test("Der Quotient aus 10 durch 3 ist nahe an 3,33", () => {
-  const result = calculator.divide(10, 3);
-
+      //Arrage
+  const [a, b] = [10, 3]
+    // Act
+  const result = calculator.divide(a, b);
+    // Assert (bzw. Expect) 
   expect(result).toBeCloseTo(3.33, 2);
 });
 
 test("Postive durch negative Zahl ist kleiner als 0", () => {
-  const result = calculator.divide(10, -2);
-
+        //Arrage
+        const [a, b] = [10, -2]
+        // Act
+  const result = calculator.divide(a, b);
+    // Assert (bzw. Expect)
   expect(result).toBeLessThan(0);
 });
 
 test("Negativ durch negative Zahl ist größer als 0", () => {
-  const result = calculator.divide(-10, -2);
-
+        //Arrage
+        const [a, b] = [-10, -2]
+        // Act
+  const result = calculator.divide(a, b);
+    // Assert (bzw. Expect)
   expect(result).toBeGreaterThan(0);
 });
 test("Subtraktion von positiven Zahlen", () => {
-  const result = calculator.subtract(5, 3);
+        //Arrage
+        const [a, b] = [5, 3]
+        // Act
+  const result = calculator.subtract(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(2);
 });
 
 test("Subtraktion von negativen Zahlen", () => {
-  const result = calculator.subtract(-10, -5);
+        //Arrage
+        const [a, b] = [-10, -5]
+        // Act
+  const result = calculator.subtract(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(-5);
 });
 
 test("Subtraktion von einer Zahl und 0", () => {
-  const result = calculator.subtract(0, 7);
+        //Arrage
+        const [a, b] = [0, 7]
+        // Act
+  const result = calculator.subtract(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(-7);
 });
 
 test("Multiplikation von positiven Zahlen", () => {
-  const result = calculator.multiply(4, 2);
+        //Arrage
+        const [a, b] = [4, 2]
+        // Act
+  const result = calculator.multiply(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(8);
 });
 
 test("Multiplikation mit einer negativen Zahl", () => {
-  const result = calculator.multiply(-3, 6);
+        //Arrage
+        const [a, b] = [-3, 6]
+        // Act
+  const result = calculator.multiply(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(-18);
 });
 
 test("Multiplikation mit einer Zahl und 0", () => {
-  const result = calculator.multiply(0, 9);
+        //Arrage
+        const [a, b] = [0, 9]
+        // Act
+  const result = calculator.multiply(a, b);
+      // Assert (bzw. Expect)
   expect(result).toBe(0);
 });
