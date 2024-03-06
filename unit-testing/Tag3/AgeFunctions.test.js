229bab6a469d57ffc4dd90aaae4bc5d06169c791
matze328 <152269645+matze328@users.noreply.github.com>
2024-03-06 14:07:04 +0100

    add tests zu age aufgabe2

--- unit-testing/Tag3/AgeFunctions.test.js
+++ unit-testing/Tag3/AgeFunctions.test.js
@@ -5,3 +5,19 @@ describe("Tests for isAdult Function", () => {
     expect(isAdult(18)).toBe(true);
   });
 });
+test('should return false if age is under 18', () => {
+    expect(isAdult(17)).toBe(false);
+    expect(isAdult(10)).toBe(false);
+  });
+
+  test('should throw a TypeError if input is not a number', () => {
+    expect(() => {
+      isAdult("not a number");
+    }).toThrow(TypeError);
+  });
+
+  test('should throw a TypeError if age is smaller than 0', () => {
+    expect(() => {
+      isAdult(-5);
+    }).toThrow(TypeError);
+  });
