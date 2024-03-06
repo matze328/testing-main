fa1b72d2ad8bac21c28508d2c6431435519524e0
matze328 <152269645+matze328@users.noreply.github.com>
2024-03-06 11:13:51 +0100

    copy datai und add new Test error

---
+++ unit-testing/Tag3/mymathfunctions.test.js
@@ -0,0 +1,24 @@
+const { addNumbers, divideNrs } = require("./MyMathFunctions");
+
+describe("Test Basic Math Add Functions", () => {
+  test("Basic Add Function to pass with Basic input", () => {
+    const result = addNumbers(2, 5);
+    console.log("My Result", result);
+    expect(result).toBe(7);
+  });
+});
+
+describe("Test Basic Math Divide Functions", () => {
+  test("Basic Divide Function to pass with Basic input", () => {
+    const result = divideNrs(10, 5);
+    console.log("MY division result", result);
+    expect(result).toBe(2);
+  });
+});
+describe("Test Basic Math Divide Functions", () => {
+    test("Basic Divide Function to pass with Basic input", () => {
+     expect(() => {
+        divideNrs(10, 0);
+     }).toThrow(TypeError);
+    });
+  });
